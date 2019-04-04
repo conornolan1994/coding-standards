9576be4a8af7e1a61a1aa9ba91bb3da111df1e91
Conor Nolan <C15757905@mydit.ie>
2019-04-04 19:42:36 +0100

    adding unit testing example

---
+++ src/test/MyMath.java
@@ -0,0 +1,18 @@
+package test;
+
+class MyMath {
+	
+	  private int a, b;
+	  
+	  MyMath(int a, int b)
+	  {
+	    this.a = a;
+	    this.b = b;
+	  }
+	  
+	  public int practice()
+	  {
+	    return a+b;
+	  }
+	  
+}
