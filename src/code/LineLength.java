356fe5a4ff60cade9886ac66c87edc4c660c943c
Conor Nolan <C15757905@mydit.ie>
2019-04-04 17:45:50 +0100

    adding line length examples

--- src/code/LineLength.java
+++ src/code/LineLength.java
@@ -1,5 +1,31 @@
 package code;
 
 public class LineLength {
+	
+	int longLengthExpression = 0;
+	
+	
+	
+	//GOOD EXAMPLE
+	public void goodLineLengthExample()
+	{
+		if ((longLengthExpression == 1) || (longLengthExpression == 2) || (longLengthExpression == 3) ||
+				(longLengthExpression == 4) || (longLengthExpression == 5))
+				{
+					//do something
+				}
+	}
+	
+	
+	
+	//BAD EXAMPLE
+		public void badLineLengthExample()
+		{
+			if ((longLengthExpression == 1) || (longLengthExpression == 2) || (longLengthExpression == 3) ||(longLengthExpression == 4) || (longLengthExpression == 5))
+			{
+				
+			}
+		}
+	
 
 }
