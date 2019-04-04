88aa31c2e833f45c22b51a598b2f4d1060f4ee35
Conor Nolan <C15757905@mydit.ie>
2019-04-04 16:09:39 +0100

    adding naming coding standards

---
+++ src/code/NamingExamples.java
@@ -0,0 +1,31 @@
+package code;
+
+//Class name
+public class NamingExamples {
+	
+	//variable example
+	int position = 0;
+	
+	//temporary variable example
+	int i = 0;
+	
+	//constant variable example
+	static final int DAYS_IN_WEEK = 7;
+	
+	//method name example
+	public void changePosition()
+	{
+		if (position == 0)
+		{
+			position = position + 1;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
