ddbb849e39434d4760ab82c29dbba085e92cfd0a
Conor Nolan <C15757905@mydit.ie>
2019-04-04 16:50:00 +0100

    adding indentation file

---
+++ src/code/IndentationExample.java
@@ -0,0 +1,24 @@
+package code;
+
+import java.util.List;
+
+public class IndentationExample {
+
+	public List list;
+	
+	public void indentation()
+	{
+		for (int i = 0; i < list.size();i++)
+		{
+			if (i == 3)
+			{
+				//do something
+			}
+			else
+			{
+				//do something else
+			}
+		}
+
+	}
+}
